f2db18e29798384a4b074432f23057f5b60933f5
Tonio Suessdorf <tonio.suessdorf@gmail.com>
2025-10-06 16:41:28 +0200

    Added toaster to routes

--- src/routes/CustomChallengeRoute.tsx
+++ src/routes/CustomChallengeRoute.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
 
 const CustomChallengeRoute: React.FC = () => {
     return (
         <div>
             {/* Custom Challenge Route where user enters their personal challenge */}
             <Outlet />
+            <Toaster />
         </div>
     );
 };
--- src/routes/RootRoute.tsx
+++ src/routes/RootRoute.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
 
 const RootRoute: React.FC = () => {
   return (
     <div>
       {/* <p>Welcome to the Root Route</p> */}
       <Outlet />
+      <Toaster />
     </div>
   );
 };
